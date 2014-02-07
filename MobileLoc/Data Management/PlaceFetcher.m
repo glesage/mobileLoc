a105a0fe29c881091f1d21854675c2624175b1dc
glesage <gefthefrench@gmail.com>
2014-02-07 09:16:55 -0600

    Pre cocoapods install

---
+++ MobileLoc/Data Management/PlaceFetcher.m
@@ -0,0 +1,20 @@
+//
+//  PlaceFetcher.m
+//  mobileloc
+//
+//  Created by ANDREW KUCHARSKI on 2/7/14.
+//  Copyright (c) 2014 GeoffroyLesage. All rights reserved.
+//
+
+#import "PlaceFetcher.h"
+
+@implementation PlaceFetcher
+
+-(id)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+@end
---
+++ MobileLoc/SettingsViewController.h
@@ -0,0 +1,23 @@
+//
+//  FlipsideViewController.h
+//  mobileloc
+//
+//  Created by GEOFFROY LESAGE on 2/7/14.
+//  Copyright (c) 2014 GeoffroyLesage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SettingsViewController;
+
+@protocol SettingsViewControllerDelegate
+- (void)settingsViewControllerDidFinish:(SettingsViewController *)controller;
+@end
+
+@interface SettingsViewController : UIViewController
+
+@property (weak, nonatomic) id <SettingsViewControllerDelegate> delegate;
+
+- (IBAction)done:(id)sender;
+
+@end
